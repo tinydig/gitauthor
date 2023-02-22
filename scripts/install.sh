a4ce052e525168df66bc62f136e9361ad299f775
Tinydig <tinydig@protonmail.com>
2023-02-22 21:59:11 +0200

    Add install script and update readme

---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+VERSION=$1
+PLATFORM=$2
+URL=https://github.com/tinydig/gitauthor/releases/download/${VERSION}/gitauthor-${PLATFORM}
+
+curl -o gitauthor URL
+chmod 755 gitauthor
+mv gitauthor /usr/local/bin
